07ce4d20cd83ed0fc75400135c4614f7757e70cc
Haizhou Wang <whz20024117@gmail.com>
2022-04-25 13:25:57 -0400

    a buggy checkpoint

--- LoopDetect/containers.h
+++ LoopDetect/containers.h
@@ -303,3 +303,199 @@ public:
     }
 
 };
+
+
+/* A simple implementation of hashtable. Essentially very similar to hash set. */
+template <class KeyT, class ValT>
+class MyMap {
+    uint64_t _size;
+    uint64_t n_buckets;
+    __BucketListItem<KeyT, ValT> **buckets; 
+
+    void free_bucket(__BucketListItem<KeyT, ValT> *bucket_node) {
+        if (!bucket_node) {
+            fprintf(stderr, "MyMap free error: bucket_node is nullptr.\n");
+            return;
+        }
+
+        __BucketListItem<KeyT, ValT> *next_node;
+        while (bucket_node->next) {
+            next_node = bucket_node->next;
+            free(bucket_node);
+            bucket_node = next_node;
+        }
+        free(bucket_node);
+    }
+
+    int insert_to_bucket(KeyT key, ValT val) {
+        __BucketListItem<KeyT, ValT> *item = (__BucketListItem<KeyT, ValT> *) malloc(sizeof(__BucketListItem<KeyT, ValT>));
+        if (!item) {
+            fprintf(stderr, "MySet insertion error: cannot allocate memory for new item.\n");
+            return -1;
+        }
+        item->key = key;
+        item->val = val;
+        item->next = nullptr;
+
+        size_t bucket_index = get_index(key);
+        if (!buckets[bucket_index]) { // no item in the list
+            buckets[bucket_index] = item;
+            return 0;
+        }
+
+        __BucketListItem<KeyT, ValT> *bucket_node = buckets[bucket_index];
+        while (bucket_node->next) {
+            if (bucket_node->key == key) {
+                free(item);
+                return 0;
+            }
+            bucket_node = bucket_node->next;
+        }
+        if (bucket_node->key == key) {
+            free(item);
+            return 0;
+        }
+            
+        bucket_node->next = item;
+        return 0;
+    }
+
+    __BucketListItem<KeyT, ValT> **buckets_enlarge_n(uint64_t new_n_buckets) {
+        // Return nullptr if failed. Return new buckets pointer on succeed.
+
+        if (new_n_buckets <= n_buckets) {
+            fprintf(stderr, "MySet bucket enlarge error: desired size is smaller than current size.\n");
+            return nullptr;
+        }
+
+        __BucketListItem<KeyT, ValT> **new_buckets = \
+            (__BucketListItem<KeyT, ValT> **) malloc(new_n_buckets * sizeof(__BucketListItem<KeyT, ValT>*));
+        if (!new_buckets) {
+            fprintf(stderr, "MySet bucket enlarge error: memory allocation failed for new buckets with number %lu.\n", new_n_buckets);
+            return nullptr;
+        }
+
+        for (size_t i=0; i < new_n_buckets; i++) {
+            new_buckets[i] = nullptr;
+        }
+
+        // From now on we need to be careful. We now change the pointers.
+        // Save old stuff.
+        __BucketListItem<KeyT, ValT> **old_buckets = buckets;
+        uint64_t old_n_buckets = n_buckets;
+
+        // New memories
+        buckets = new_buckets;
+        n_buckets = new_n_buckets;
+
+        for (size_t i=0; i < old_n_buckets; i++) {
+            __BucketListItem<KeyT, ValT> *tmp_bucket = old_buckets[i];
+
+            while (tmp_bucket) {
+                if (insert_to_bucket(tmp_bucket->key, tmp_bucket->val)) {
+                    goto failed;
+                }
+                tmp_bucket = tmp_bucket->next;
+            }
+        }
+
+        // Now clean up old memories
+        for (size_t i=0; i < old_n_buckets; i++) {
+            __BucketListItem<KeyT, ValT> *tmp_bucket = old_buckets[i];
+
+            if (tmp_bucket) {
+                free_bucket(tmp_bucket);
+            }
+        }
+        free(old_buckets);
+        return buckets;
+
+    failed:
+        // if failed, we clean up newly allocated memories.
+        buckets = old_buckets;
+        n_buckets = old_n_buckets;
+
+        for (size_t i=0; i < new_n_buckets; i++) {
+            __BucketListItem<KeyT, ValT> *tmp_bucket = new_buckets[i];
+
+            if (tmp_bucket) {
+                free_bucket(tmp_bucket);
+            }
+        }
+        free(new_buckets);
+        
+        return nullptr;
+    }
+
+    inline uint64_t get_index_from_hash(size_t hash) {
+        return hash % n_buckets;
+    }
+
+    uint64_t get_index(KeyT key) {
+        size_t hash = std::hash<KeyT>{}(key);
+        return get_index_from_hash(hash);
+    }
+
+public:
+    MyMap() {
+        _size = 0;
+        n_buckets = __bucket_number_prime_list[0];
+        buckets = (__BucketListItem<KeyT, ValT> **) malloc(n_buckets * sizeof(__BucketListItem<KeyT, ValT>*));
+        if (!buckets) {
+            fprintf(stderr, "MySet bucket initial error: memory allocation failed for buckets.\n");
+            exit(-1);
+        }
+        for (size_t i=0; i < n_buckets; i++) {
+            buckets[i] = nullptr;
+        }
+    }
+
+    ~MyMap() {
+        for (size_t i=0; i < n_buckets; i++) {
+            if (buckets[i])
+                free_bucket(buckets[i]);
+        }
+        free(buckets);
+    }
+
+    void insert(KeyT key, ValT val) {
+        if (insert_to_bucket(key, val)) {
+            return;
+        }
+        _size++;
+
+        if (_size > n_buckets / 2) {
+            unsigned idx = 0;
+            uint64_t new_n_bucket = __bucket_number_prime_list[idx];
+
+            while (new_n_bucket <= n_buckets) {
+                new_n_bucket = __bucket_number_prime_list[++idx];
+                if (idx == 28) {
+                    fprintf(stderr, "MySet insertion warning: cannot enlarge anymore.\n");
+                    return;
+                }
+            }
+
+            buckets_enlarge_n(new_n_bucket);
+        }
+    }
+
+    ValT& operator[](KeyT key) {
+        uint64_t index = get_index(key);
+        __BucketListItem<KeyT, ValT> *bucket_node = buckets[index];
+        if (!bucket_node) {
+            fprintf(stderr, "MySet access error: cannot find key.\n");
+            exit(-1);
+        }
+
+        while (bucket_node) {
+            if (bucket_node->key == key) {
+                return bucket_node->val;
+            }
+            bucket_node = bucket_node->next;
+        }
+        fprintf(stderr, "MySet access error: cannot find key.\n");
+        exit(-1);
+    }
+
+};
